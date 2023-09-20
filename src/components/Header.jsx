18869b95ffc3c83eaaee066e60887ac4cb8c6b33
BuuBuu <chaungocbuudang2003@gmail.com>
2023-09-20 22:08:26 +0700

    update fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className='fixed w-full'>
             <div className="w-full h-9 p-2.5 justify-end items-center gap-12 inline-flex ">
-                <div className="text-black text-sm font-normal font-['SF Pro Display']">Đăng nhập</div>
-                <div className="text-black text-sm font-normal font-['SF Pro Display'] pr-8">Wishlist</div>
+                <div className="text-black text-sm font-normal font-SFPro">Đăng nhập</div>
+                <div className="text-black text-sm font-normal font-SFPro pr-8">Wishlist</div>
             </div>
             <div className="w-full h-16 p-2.5 bg-black justify-center items-center gap-72 inline-flex">
-                <div className="text-white text-xl font-normal font-['SF Pro Display']">
-
+                <div className="text-white text-xl font-SFPro">
                     Apple Store</div>
                 <div className="p-2.5 justify-center items-center gap-16 flex">
-                    <div className="text-white text-base font-normal font-['SF Pro Display']">Trang chủ</div>
-                    <div className="text-white text-base font-normal font-['SF Pro Display']">Phân loại</div>
-                    <div className="text-white text-base font-normal font-['SF Pro Display']">Sắp ra mắt</div>
-                    <div className="text-white text-base font-normal font-['SF Pro Display']">Blog</div>
+                    <Link to='' className="text-white text-base font-normal font-SFPro hover:text-gray">Trang chủ</Link>
+                    <Link className="text-white text-base font-normal font-SFPro hover:text-gray ">Phân loại</Link>
+                    <Link className="text-white text-base font-normal font-SFPro hover:text-gray">Sắp ra mắt</Link>
+                    <Link className="text-white text-base font-normal font-SFPro hover:text-gray">Blog</Link>
                 </div>
                 <div className="p-2.5 justify-center items-center gap-5 flex">
                     <div className="h-5 px-1 py-0.5 justify-start items-end gap-1.5 flex">
-                        <ShoppingCartIcon color='#fff'></ShoppingCartIcon>
+                        <ShoppingCartIcon className='text-white'></ShoppingCartIcon>
                     </div>
-                    <div className="text-white text-sm font-normal font-['SF Pro Display']">0</div>
+                    <div className="text-white font-16 ont-normal font-SFPro">0</div>
                 </div>
             </div>
-        </div>
+        </div >
 
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    colors: {
+      'blue': '#1875e4',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'red': '#fe6351',
+      'gray-dark': '#444444',
+      'gray': '#8492a6',
+      'gray-light': '#eff1f2',
+      'black': '#000000',
+      'white': '#fff'
+    },
+    fontFamily: {
+      'SFPro': ['SFPro', 'Arial']
+    },
   },
   plugins: [require("daisyui")],
+
 }
