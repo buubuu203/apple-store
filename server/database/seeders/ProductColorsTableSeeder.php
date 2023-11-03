8ee6e2dd835af2d23c8f5af7f294cbaff57f1c74
LTThuyLinh <letranthuylinh1003@gmail.com>
2023-11-03 14:12:31 +0700

    update ProductColorsTableSeeder

--- server/database/seeders/ProductColorsTableSeeder.php
+++ server/database/seeders/ProductColorsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+//php artisan db:seed --class=ProductColorsTableSeeder
 
 class ProductColorsTableSeeder extends Seeder
 {
@@ -14,5 +15,725 @@ class ProductColorsTableSeeder extends Seeder
     public function run()
     {
         //
+        $colors = [
+            //Iphone
+            //product_id=1
+            [
+                'product_id' => 1,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 1,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 1,
+                'name' => 'Bạc',
+            ],
+            //product_id=2
+            [
+                'product_id' => 2,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 2,
+                'name' => 'Trắng',
+            ],
+            [
+                'product_id' => 2,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 2,
+                'name' => 'Đen',
+            ],
+            //product_id=3
+            [
+                'product_id' => 3,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 3,
+                'name' => 'Bạc',
+            ],
+            //product_id=4
+            [
+                'product_id' => 4,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 4,
+                'name' => 'Bạc',
+            ],
+            //product_id = 5
+            [
+                'product_id' => 5,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 5,
+                'name' => 'Bạc',
+            ],
+            //product_id = 6
+            [
+                'product_id' => 6,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 6,
+                'name' => 'Bạc',
+            ],
+            //product_id = 7
+            [
+                'product_id' => 7,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 7,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 7,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 7,
+                'name' => 'Xanh đen',
+            ],
+            //product_id = 8
+            [
+                'product_id' => 8,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 8,
+                'name' => 'Bạc',
+            ],
+            //product_id = 9
+            [
+                'product_id' => 9,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 9,
+                'name' => 'Bạc',
+            ],
+            //product_id = 10
+            [
+                'product_id' => 10,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 10,
+                'name' => 'Bạc',
+            ],
+            //product_id = 11
+            [
+                'product_id' => 11,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 11,
+                'name' => 'Bạc',
+            ],
+            //Iphone
+            //product_id = 12 //1
+            [
+                'product_id' => 12,
+                'name' => 'Trắng',
+            ],
+            [
+                'product_id' => 12,
+                'name' => 'Đen',
+            ],
+            [
+                'product_id' => 12,
+                'name' => 'Tím',
+            ],
+            [
+                'product_id' => 12,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 12,
+                'name' => 'Đỏ',
+            ],
+            [
+                'product_id' => 12,
+                'name' => 'Xanh',
+            ],
+            //product_id = 13 //2
+            [
+                'product_id' => 13,
+                'name' => 'Trắng',
+            ],
+            [
+                'product_id' => 13,
+                'name' => 'Đen',
+            ],
+            [
+                'product_id' => 13,
+                'name' => 'Tím',
+            ],
+            [
+                'product_id' => 13,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 13,
+                'name' => 'Đỏ',
+            ],
+            [
+                'product_id' => 13,
+                'name' => 'Xanh',
+            ],
+            //product_id = 14 //3
+            [
+                'product_id' => 14,
+                'name' => 'Trắng',
+            ],
+            [
+                'product_id' => 14,
+                'name' => 'Đen',
+            ],
+            [
+                'product_id' => 14,
+                'name' => 'Tím',
+            ],
+            [
+                'product_id' => 14,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 14,
+                'name' => 'Đỏ',
+            ],
+            [
+                'product_id' => 14,
+                'name' => 'Xanh',
+            ],
+            //product_id = 15 //4
+            [
+                'product_id' => 15,
+                'name' => 'Trắng',
+            ],
+            [
+                'product_id' => 15,
+                'name' => 'Đen',
+            ],
+            [
+                'product_id' => 15,
+                'name' => 'Tím',
+            ],
+            [
+                'product_id' => 15,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 15,
+                'name' => 'Đỏ',
+            ],
+            [
+                'product_id' => 15,
+                'name' => 'Xanh',
+            ],
+            //product_id = 16 //5
+            [
+                'product_id' => 16,
+                'name' => 'Trắng',
+            ],
+            [
+                'product_id' => 16,
+                'name' => 'Đen',
+            ],
+            [
+                'product_id' => 16,
+                'name' => 'Tím',
+            ],
+            [
+                'product_id' => 16,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 16,
+                'name' => 'Đỏ',
+            ],
+            [
+                'product_id' => 16,
+                'name' => 'Xanh',
+            ],
+            //product_id = 17 //6
+            [
+                'product_id' => 17,
+                'name' => 'Tím',
+            ],
+            [
+                'product_id' => 17,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 17,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 17,
+                'name' => 'Bạc',
+            ],
+            //product_id = 18 //7
+            [
+                'product_id' => 18,
+                'name' => 'Tím',
+            ],
+            [
+                'product_id' => 18,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 18,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 18,
+                'name' => 'Bạc',
+            ],
+            //product_id = 19 //8
+            [
+                'product_id' => 19,
+                'name' => 'Tím',
+            ],
+            [
+                'product_id' => 19,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 19,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 19,
+                'name' => 'Bạc',
+            ],
+            //product_id = 20 //9
+            [
+                'product_id' => 20,
+                'name' => 'Tím',
+            ],
+            [
+                'product_id' => 20,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 20,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 20,
+                'name' => 'Bạc',
+            ],
+            //product_id = 21 //10
+            [
+                'product_id' => 21,
+                'name' => 'Tím',
+            ],
+            [
+                'product_id' => 21,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 21,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 21,
+                'name' => 'Bạc',
+            ],
+            //product_id = 22 //11
+            [
+                'product_id' => 22,
+                'name' => 'Tím',
+            ],
+            [
+                'product_id' => 22,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 22,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 22,
+                'name' => 'Bạc',
+            ],
+            //product_id = 23 //12
+            [
+                'product_id' => 23,
+                'name' => 'Titan Xanh',
+            ],
+            [
+                'product_id' => 23,
+                'name' => 'Titan Đen',
+            ],
+            [
+                'product_id' => 23,
+                'name' => 'Titan Trắng',
+            ],
+            [
+                'product_id' => 23,
+                'name' => 'Titan tự nhiên',
+            ],
+            //product_id = 24 //13
+            [
+                'product_id' => 24,
+                'name' => 'Xanh dương',
+            ],
+            [
+                'product_id' => 24,
+                'name' => 'Vàng',
+            ],
+            [
+                'product_id' => 24,
+                'name' => 'Đen',
+            ],
+            [
+                'product_id' => 24,
+                'name' => 'Xanh lá',
+            ],
+            [
+                'product_id' => 24,
+                'name' => 'Hồng',
+            ],
+            //Ipad
+            //product_id = 25 //1
+            [
+                'product_id' => 25,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 25,
+                'name' => 'Xanh',
+            ],
+            [
+                'product_id' => 24,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 24,
+                'name' => 'Vàng',
+            ],
+            //product_id = 26 //2
+            [
+                'product_id' => 26,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 26,
+                'name' => 'Xanh',
+            ],
+            [
+                'product_id' => 26,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 26,
+                'name' => 'Vàng',
+            ],
+            //product_id = 27 //3
+            [
+                'product_id' => 27,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 27,
+                'name' => 'Xanh',
+            ],
+            [
+                'product_id' => 27,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 27,
+                'name' => 'Vàng',
+            ],
+            //product_id = 28 //4
+            [
+                'product_id' => 28,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 28,
+                'name' => 'Xanh',
+            ],
+            [
+                'product_id' => 28,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 28,
+                'name' => 'Vàng',
+            ],
+            //product_id = 29 //5
+            [
+                'product_id' => 29,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 29,
+                'name' => 'Xanh',
+            ],
+            [
+                'product_id' => 29,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 29,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 29,
+                'name' => 'Tím',
+            ],
+            //product_id = 30 //6
+            [
+                'product_id' => 30,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 30,
+                'name' => 'Xanh',
+            ],
+            [
+                'product_id' => 30,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 30,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 30,
+                'name' => 'Tím',
+            ],
+            //product_id = 31 //7
+            [
+                'product_id' => 31,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 31,
+                'name' => 'Xanh',
+            ],
+            [
+                'product_id' => 31,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 31,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 31,
+                'name' => 'Tím',
+            ],
+            //product_id = 32 //8
+            [
+                'product_id' => 32,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 32,
+                'name' => 'Xanh',
+            ],
+            [
+                'product_id' => 32,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 32,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 32,
+                'name' => 'Tím',
+            ],
+            //product_id = 33 //9
+            [
+                'product_id' => 33,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 33,
+                'name' => 'Bạc',
+            ],
+            //product_id = 34 //10
+            [
+                'product_id' => 34,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 34,
+                'name' => 'Bạc',
+            ],
+            //product_id = 35 //11
+            [
+                'product_id' => 35,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 35,
+                'name' => 'Bạc',
+            ],
+             //product_id = 36 //12
+             [
+                'product_id' => 36,
+                'name' => 'Xám',
+            ],
+            [
+                'product_id' => 36,
+                'name' => 'Bạc',
+            ],
+            //product_id = 37 //13
+            //product_id = 38 //14
+            [
+                'product_id' => 38,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 38,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 38,
+                'name' => 'Xám đen',
+            ],
+            [
+                'product_id' => 38,
+                'name' => 'Tím',
+            ],
+             //product_id = 39 //15
+             [
+                'product_id' => 39,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 39,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 39,
+                'name' => 'Xám đen',
+            ],
+            [
+                'product_id' => 39,
+                'name' => 'Tím',
+            ],
+             //product_id = 40 //16
+             [
+                'product_id' => 40,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 40,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 40,
+                'name' => 'Xám đen',
+            ],
+            [
+                'product_id' => 40,
+                'name' => 'Tím',
+            ],
+             //product_id = 41 //17
+             [
+                'product_id' => 41,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 41,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 41,
+                'name' => 'Xám đen',
+            ],
+            [
+                'product_id' => 41,
+                'name' => 'Tím',
+            ],
+            //product_id = 42 //18
+            [
+                'product_id' => 42,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 42,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 42,
+                'name' => 'Xám đen',
+            ],
+            [
+                'product_id' => 42,
+                'name' => 'Tím',
+            ],
+            //product_id = 43 //19
+            [
+                'product_id' => 43,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 43,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 43,
+                'name' => 'Xám đen',
+            ],
+            [
+                'product_id' => 43,
+                'name' => 'Tím',
+            ],
+            //product_id = 44 //20
+            [
+                'product_id' => 44,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 44,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 44,
+                'name' => 'Xám đen',
+            ],
+            [
+                'product_id' => 44,
+                'name' => 'Tím',
+            ],
+            //product_id = 45 //21
+            [
+                'product_id' => 45,
+                'name' => 'Hồng',
+            ],
+            [
+                'product_id' => 45,
+                'name' => 'Bạc',
+            ],
+            [
+                'product_id' => 45,
+                'name' => 'Xám đen',
+            ],
+            [
+                'product_id' => 45,
+                'name' => 'Tím',
+            ],
+            //Airpod
+            //product_id = 46 //1
+
+        ];
+        foreach ($colors as $color) {
+            ProductColor::create($color);
+        }
     }
 }
